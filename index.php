614c72400b55e7008134ec599f7c111f9f040a70
Kelly Santos <105869919+AnakellySantosMascarenhas@users.noreply.github.com>
2023-09-11 17:49:55 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> <html lang="pt-br"> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <title>Média</title> 
+    </head> 
+    
+    <body> 
+        <form action="resultado.php" method="post"> Calcule a Média : 
+            <br> 
+            Digite sua primeira nota: <input type="Textarea" name="primeira"><br>  
+            Digite sua segunda nota:<input type="Textarea" name="segunda"><br>  
+            Digite sua terceira nota:<input type="Textarea" name="terceira"><br>
+            Digite sua quarta nota:<input type="Textarea" name="quarta"><br> 
+            <input type="submit"> 
+        </form> 
+    </body> 
+    </html>
---
+++ resultado.php
@@ -0,0 +1,8 @@
+<?php 
+$primeira = $_POST["primeira"]; 
+$segunda =  $_POST["segunda"];  
+$terceira = $_POST["terceira"]; 
+$quarta = $_POST["quarta"]; 
+$resultado = ($primeira + $segunda + $terceira + $quarta)/ 4; echo $resultado; 
+
+?>
